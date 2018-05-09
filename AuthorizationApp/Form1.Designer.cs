b9f3deff7a96b9c2a7724d6cfb4979782aabf7d3
TheBoyCode <olexandermelnyk@gmail.com>
2018-05-09 18:23:33 +0200

    Add register form , main form

---
+++ AuthorizationApp/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AuthorizationApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Log = new System.Windows.Forms.Button();
+            this.button_Reg = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_Log
+            // 
+            this.button_Log.Location = new System.Drawing.Point(276, 118);
+            this.button_Log.Name = "button_Log";
+            this.button_Log.Size = new System.Drawing.Size(193, 60);
+            this.button_Log.TabIndex = 0;
+            this.button_Log.Text = "Логін";
+            this.button_Log.UseVisualStyleBackColor = true;
+            this.button_Log.Click += new System.EventHandler(this.button_Log_Click);
+            // 
+            // button_Reg
+            // 
+            this.button_Reg.Location = new System.Drawing.Point(276, 249);
+            this.button_Reg.Name = "button_Reg";
+            this.button_Reg.Size = new System.Drawing.Size(193, 60);
+            this.button_Reg.TabIndex = 1;
+            this.button_Reg.Text = "Реєстрація";
+            this.button_Reg.UseVisualStyleBackColor = true;
+            this.button_Reg.Click += new System.EventHandler(this.button_Reg_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_Reg);
+            this.Controls.Add(this.button_Log);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Log;
+        private System.Windows.Forms.Button button_Reg;
+    }
+}
+
---
+++ AuthorizationApp/Form2.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace AuthorizationApp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label_name = new System.Windows.Forms.Label();
+            this.label_lastname = new System.Windows.Forms.Label();
+            this.label_numb = new System.Windows.Forms.Label();
+            this.label_login = new System.Windows.Forms.Label();
+            this.label_passw = new System.Windows.Forms.Label();
+            this.label_email = new System.Windows.Forms.Label();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.textBox_lastname = new System.Windows.Forms.TextBox();
+            this.textBox_numb = new System.Windows.Forms.TextBox();
+            this.textBox_login = new System.Windows.Forms.TextBox();
+            this.textBox_passw = new System.Windows.Forms.TextBox();
+            this.textBox_email = new System.Windows.Forms.TextBox();
+            this.button_OK_Reg = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 32.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67.5F));
+            this.tableLayoutPanel1.Controls.Add(this.label_name, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label_lastname, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label_numb, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label_login, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label_passw, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label_email, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_name, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_lastname, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_numb, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_login, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_passw, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_email, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.button_OK_Reg, 1, 6);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label_name
+            // 
+            this.label_name.AutoSize = true;
+            this.label_name.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_name.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_name.Location = new System.Drawing.Point(31, 31);
+            this.label_name.Margin = new System.Windows.Forms.Padding(31, 31, 0, 0);
+            this.label_name.Name = "label_name";
+            this.label_name.Size = new System.Drawing.Size(229, 33);
+            this.label_name.TabIndex = 0;
+            this.label_name.Text = "Ім\'я";
+            this.label_name.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label_lastname
+            // 
+            this.label_lastname.AutoSize = true;
+            this.label_lastname.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_lastname.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_lastname.Location = new System.Drawing.Point(31, 95);
+            this.label_lastname.Margin = new System.Windows.Forms.Padding(31, 31, 0, 0);
+            this.label_lastname.Name = "label_lastname";
+            this.label_lastname.Size = new System.Drawing.Size(229, 33);
+            this.label_lastname.TabIndex = 1;
+            this.label_lastname.Text = "Прізвище";
+            // 
+            // label_numb
+            // 
+            this.label_numb.AutoSize = true;
+            this.label_numb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_numb.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_numb.Location = new System.Drawing.Point(31, 159);
+            this.label_numb.Margin = new System.Windows.Forms.Padding(31, 31, 0, 0);
+            this.label_numb.Name = "label_numb";
+            this.label_numb.Size = new System.Drawing.Size(229, 33);
+            this.label_numb.TabIndex = 2;
+            this.label_numb.Text = "Номер телефону";
+            // 
+            // label_login
+            // 
+            this.label_login.AutoSize = true;
+            this.label_login.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_login.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_login.Location = new System.Drawing.Point(31, 223);
+            this.label_login.Margin = new System.Windows.Forms.Padding(31, 31, 0, 0);
+            this.label_login.Name = "label_login";
+            this.label_login.Size = new System.Drawing.Size(229, 33);
+            this.label_login.TabIndex = 3;
+            this.label_login.Text = "Логін";
+            // 
+            // label_passw
+            // 
+            this.label_passw.AutoSize = true;
+            this.label_passw.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_passw.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_passw.Location = new System.Drawing.Point(31, 287);
+            this.label_passw.Margin = new System.Windows.Forms.Padding(31, 31, 0, 0);
+            this.label_passw.Name = "label_passw";
+            this.label_passw.Size = new System.Drawing.Size(229, 33);
+            this.label_passw.TabIndex = 4;
+            this.label_passw.Text = "Пароль";
+            // 
+            // label_email
+            // 
+            this.label_email.AutoSize = true;
+            this.label_email.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_email.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_email.Location = new System.Drawing.Point(31, 351);
+            this.label_email.Margin = new System.Windows.Forms.Padding(31, 31, 0, 0);
+            this.label_email.Name = "label_email";
+            this.label_email.Size = new System.Drawing.Size(229, 33);
+            this.label_email.TabIndex = 5;
+            this.label_email.Text = "Ел. пошта";
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_name.Location = new System.Drawing.Point(263, 30);
+            this.textBox_name.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(428, 34);
+            this.textBox_name.TabIndex = 6;
+            // 
+            // textBox_lastname
+            // 
+            this.textBox_lastname.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_lastname.Location = new System.Drawing.Point(263, 94);
+            this.textBox_lastname.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.textBox_lastname.Name = "textBox_lastname";
+            this.textBox_lastname.Size = new System.Drawing.Size(428, 34);
+            this.textBox_lastname.TabIndex = 7;
+            // 
+            // textBox_numb
+            // 
+            this.textBox_numb.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_numb.Location = new System.Drawing.Point(263, 158);
+            this.textBox_numb.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.textBox_numb.Name = "textBox_numb";
+            this.textBox_numb.Size = new System.Drawing.Size(428, 34);
+            this.textBox_numb.TabIndex = 8;
+            // 
+            // textBox_login
+            // 
+            this.textBox_login.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_login.Location = new System.Drawing.Point(263, 222);
+            this.textBox_login.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.textBox_login.Name = "textBox_login";
+            this.textBox_login.Size = new System.Drawing.Size(428, 34);
+            this.textBox_login.TabIndex = 9;
+            // 
+            // textBox_passw
+            // 
+            this.textBox_passw.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_passw.Location = new System.Drawing.Point(263, 286);
+            this.textBox_passw.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.textBox_passw.Name = "textBox_passw";
+            this.textBox_passw.Size = new System.Drawing.Size(428, 34);
+            this.textBox_passw.TabIndex = 10;
+            // 
+            // textBox_email
+            // 
+            this.textBox_email.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_email.Location = new System.Drawing.Point(263, 350);
+            this.textBox_email.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.textBox_email.Name = "textBox_email";
+            this.textBox_email.Size = new System.Drawing.Size(428, 34);
+            this.textBox_email.TabIndex = 11;
+            // 
+            // button_OK_Reg
+            // 
+            this.button_OK_Reg.Location = new System.Drawing.Point(263, 414);
+            this.button_OK_Reg.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.button_OK_Reg.Name = "button_OK_Reg";
+            this.button_OK_Reg.Size = new System.Drawing.Size(263, 33);
+            this.button_OK_Reg.TabIndex = 12;
+            this.button_OK_Reg.Text = "OK";
+            this.button_OK_Reg.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form2";
+            this.Text = "Регістрація";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label_name;
+        private System.Windows.Forms.Label label_lastname;
+        private System.Windows.Forms.Label label_numb;
+        private System.Windows.Forms.Label label_login;
+        private System.Windows.Forms.Label label_passw;
+        private System.Windows.Forms.Label label_email;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.TextBox textBox_lastname;
+        private System.Windows.Forms.TextBox textBox_numb;
+        private System.Windows.Forms.TextBox textBox_login;
+        private System.Windows.Forms.TextBox textBox_passw;
+        private System.Windows.Forms.TextBox textBox_email;
+        private System.Windows.Forms.Button button_OK_Reg;
+    }
+}
